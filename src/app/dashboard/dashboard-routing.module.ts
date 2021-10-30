1ae17f4d2bf216e2655e267f80c8994ad845b641
Ahsan <ibadahsan601@gmail.com>
2021-10-30 22:26:56 +0700

    [AHSAN] Finish Donations System on Dashboard

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,11 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  {
-    path: '',
-    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
-  },
+  { path: '', component: DashboardComponent },
 ];
 
 @NgModule({
