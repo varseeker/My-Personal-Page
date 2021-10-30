185fa000c4edd6d273490998dd27bfc1e46fbda5
Ahsan <ibadahsan601@gmail.com>
2021-10-31 00:49:38 +0700

    [AHSAN] Rest first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,6 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
   },
-  {
-    path: 'blog',
-    loadChildren: () =>
-      import('./pages/blog/blog.module').then((m) => m.BlogModule),
-  },
-  {
-    path: 'contact',
-    loadChildren: () =>
-      import('./pages/contact/contact.module').then((m) => m.ContactModule),
-  },
   { path: 'dashboard', canActivate:[RouteGuard], loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'guest', canActivate:[RouteGuard] , loadChildren: () => import('./dashboard/guest-book/guest-book.module').then(m => m.GuestBookModule) },
   { path: 'donate', canActivate:[RouteGuard] ,loadChildren: () => import('./dashboard/donations/donations.module').then(m => m.DonationsModule) },
