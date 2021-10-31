960e97b190e3f8739c9ab4ab8877fcbb3c425f99
Muhammad <mhmmdyanuar.06@gmail.com>
2021-10-31 14:45:42 +0700

    [YANUAR] able to show blog

--- src/app/pages/blog-public/blog.module.ts
+++ src/app/pages/blog-public/blog.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
-import { BlogComponent } from './blog.component';
-import { ListBlogComponent } from './component/list-blog/list-blog.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BlogService } from 'src/app/dashboard/blog/service/blog.service';
+import { BlogPublicComponent } from './blog.component';
+import { ListBlogComponent } from './components/list-blog/list-blog.component';
 import { DetailsComponent } from './components/details/details.component';
 
 
 @NgModule({
   declarations: [
-    BlogComponent,
+    BlogPublicComponent,
     ListBlogComponent,
-    DetailsComponent,
+    DetailsComponent
   ],
   imports: [
     CommonModule,
